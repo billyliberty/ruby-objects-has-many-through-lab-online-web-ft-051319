e8687ae8dee60777220ee21530a4b05ec28cc47f
Joshua Crum <joshua.crum@gmail.com>
2019-05-30 16:00:12 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,11 @@
 class Genre
 
-  attr_accessor :name, :genre, :song
+  attr_accessor :name, :genre, :songs
 
   def initialize (name)
     @name = name
+    @genre = genre
+    @songs = []
   end
 
   @@all = []
