f3bfc41389f2b1650a8a5e88db023c2a29684b18
Joshua Crum <joshua.crum@gmail.com>
2019-05-30 16:18:55 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,14 +1,18 @@
-class Doctor
 
-  attr_accessor :name, :appointment, :patient
+class Doctor
+  attr_accessor :name
 
   @@all = []
 
-  def initialize (name)
+  def initialize(name)
     @name = name
     @@all << self
   end
 
+  def self.all
+    @@all
+  end
+  
   def new_appointment (patient, date)
     Appointment.new(patient, self, date)
   end
