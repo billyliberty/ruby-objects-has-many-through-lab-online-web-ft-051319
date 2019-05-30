28400b50792d0fe26f0ec06523c9a632f3f10859
Joshua Crum <joshua.crum@gmail.com>
2019-05-30 16:01:15 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,29 +1,21 @@
 class Genre
+  attr_accessor :name, :songs
 
-  attr_accessor :name, :genre, :songs
-
-  def initialize (name)
+  def initialize(name)
     @name = name
-    @genre = genre
     @songs = []
   end
 
-  @@all = []
-
-  def self.all
-    @@all
+  def add_song(song)
+    @songs << song
   end
 
   def songs
-    Song.all.select do |song|
-      song.genre == self
-    end
+    @songs
   end
 
   def artists
-    Song.all.map do |song|
-      song.artist
-    end
+    @songs.collect { |song| song.artist }
   end
-
+  
 end
